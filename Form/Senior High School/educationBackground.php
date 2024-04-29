850a10dc10c8d6bb8e281d6eca5e7b6c74c01cf1
kastilyojl <castillojlb0922@gmail.com>
2024-04-29 12:48:56 +0800

    Added PHP File: educationBackground - SHS

---
+++ Form/Senior High School/educationBackground.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Education Background</title>
+    <link rel="stylesheet" href="./Registration-form.css">
+</head>
+<body>
+
+<div class="form">
+        <div class="header">
+            <h3>Education Background</h3>
+        </div>
+        <form action="">
+            <div class="row">
+                <label>High School (G10)</label>
+                <input type="text" placeholder="Name of school">
+                <div class="row column">
+                    <div>
+                        <label>Year Level</label>
+                        <input type="text" placeholder="E.g. Grade 10">
+                    </div>
+                    <div>
+                        <label>Year Graduated</label>
+                        <input type="text" placeholder="E.g. 2024">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <label>High School (G12)</label>
+                <input type="text" placeholder="Name of school">
+                <div class="row column">
+                    <div>
+                        <label>Year Level</label>
+                        <input type="text" placeholder="E.g. Grade 12">
+                    </div>
+                    <div>
+                        <label>Year Graduated</label>
+                        <input type="text" placeholder="E.g. 2024">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <label>TESDA NC</label>
+                <input type="text" placeholder="Name of school">
+                <div class="row column">
+                    <div>
+                        <label>Year Level</label>
+                        <input type="text" placeholder="">
+                    </div>
+                    <div>
+                        <label>Year Graduated</label>
+                        <input type="text" placeholder="E.g. 2024">
+                    </div>
+                </div>
+            </div>
+    
+        </form>
+        <div class="footer">
+            <a href="./Parents-Info.php">
+                <button class="back-button">Back</button>
+            </a>
+            <a href="./SHS-Requirements.php">
+                <button class="next-button">Next</button>
+            </a>
+        </div>
+    </div>
+    
+</body>
+</html>
