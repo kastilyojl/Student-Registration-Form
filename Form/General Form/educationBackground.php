615ce00892ca58d2717067afc71271f9a89b478a
kastilyojl <castillojlb0922@gmail.com>
2024-04-29 12:56:27 +0800

    Added PHP File: educationBackground - General

--- Form/General Form/educationBackground.php
+++ Form/General Form/educationBackground.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Education Background</title>
-    <link rel="stylesheet" href="./Registration-form.css">
+    <link rel="stylesheet" href="../../Style/registrationForm.css">
 </head>
 <body>
 
