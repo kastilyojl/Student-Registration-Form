bf13f8fe2900b4259959844958dc938a170b4e7c
kastilyojl <castillojlb0922@gmail.com>
2024-04-29 12:59:18 +0800

    Added PHP File: requirement - SHS

---
+++ Form/Senior High School/requirement.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../Style/registrationForm.css">
+</head>
+<body>
+
+    <style> 
+        .requirements {
+            padding: 20px;
+        }
+
+        p {
+            height: 30px;
+        }
+    </style>
+
+<div class="form">
+            <div class="header">
+                <h3>Requirements</h3>
+            </div>
+            <div class="requirements">
+                <p>Sample Requirement</p>
+                <p>Sample Requirement</p>
+                <p>Sample Requirement</p>
+                <p>Sample Requirement</p>
+                <p>Sample Requirement</p>
+                <p>Sample Requirement</p>
+                <p>Sample Requirement</p>
+                <p>Sample Requirement</p>
+            </div>
+            <div class="footer">
+                <a href="#">
+                    <button class="back-button">Back</button>
+                </a>
+                <a href="#">
+                    <button class="next-button">Submit</button>
+                </a>
+            </div>
+        </div>
+    
+</body>
+</html>
