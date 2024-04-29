1d1573df87641f91ee66c17d26a2868511d10b9e
kastilyojl <castillojlb0922@gmail.com>
2024-04-29 12:33:48 +0800

    Added PHP Files: parentInformation - General

---
+++ Form/General Form/parentProfile.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parent Profile</title>
+
+    <link rel="stylesheet" href="../../Style/registrationForm.css">
+    
+</head>
+<body>
+
+    <div class="form">
+            <div class="header">
+                <h3>Parent Profile</h3>
+            </div>
+            <form action="">
+                <div class="row column">
+                    <div>
+                        <label>Mother</label>
+                        <input type="text" placeholder="Mother's Name" name="mother">
+                    </div>
+                    <div>
+                        <label>Father</label>
+                        <input type="text" placeholder="Father's Name" name="father">
+                    </div>
+                </div>
+                <div class="row column">
+                    <div>
+                        <label>Mother's Occupation</label>
+                        <input type="text" placeholder="Mother's Occupation" name="mother-occupation">
+                    </div>
+                    <div>
+                        <label>Father's Occupation</label>
+                        <input type="text" placeholder="Father's Occupation" name="father-occupation">
+                    </div>
+                </div>
+                <div class="row column">
+                    <div>
+                        <label>Mother's Cellphone #</label>
+                        <input type="tel" placeholder="09123456789" name="mother-cell#">
+                    </div>
+                    <div>
+                        <label>Father's Cellphone #</label>
+                        <input type="text" placeholder="09123456789" name="father-cell#">
+                    </div>
+                </div>
+                
+            </form>
+            <div class="footer">
+                <a href="#">
+                    <button class="back-button">Back</button>
+                </a>
+                <a href="#">
+                    <button class="next-button">Next</button>
+                </a>
+            </div>
+        </div>
+
+</body>
+</html>
